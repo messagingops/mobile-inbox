286d0fb39e64022490a7185646001413032522c9
aryangorwade <gorwadearyan@gmail.com>
2024-03-04 04:52:09 -0800

    first commit with working create/edit and get routes!

--- backend/src/routes/lists.ts
+++ backend/src/routes/lists.ts
@@ -1,22 +1,104 @@
 import express, { Router, Request, Response } from "express";
-const router: Router = Router()
+import apolloClient from '../apolloClient';
+import { gql } from '@apollo/client/core';
+
+const router: Router = Router();
 
 router.use(express.urlencoded({ extended: false }));
+router.use(express.json())
 
-router.post('/', (req: Request, res: Response) => {
-   res.send('POST Request sent to lists')
-})
+// GraphQL list mutation syntax
+const LIST_MUTATION = gql`mutation createOrUpdateList(
+  $primaryPhone: String!,
+  $listQuery: ListQuery!
+) {
+  createOrUpdateList(
+    primaryPhone: $primaryPhone,
+    listQuery: $listQuery
+  ) {
+    id
+    createdBy
+    name
+    createdAt
+    lastUpdatedAt
+    size
+    querySourceListName
+    queryNameSearchTerm
+  }
+}
+`
 
-router.get('/', (req: Request, res: Response) => {
-   res.send('GET Request sent to lists')
-})
+// Graphql list query syntax
+const LIST_QUERY = gql`
+query list(
+  $primaryPhone: String!,
+  $listName: String!
+) {
+  list(
+    primaryPhone: $primaryPhone,
+    listName: $listName
+  ) {
+    id
+    createdBy
+    name
+    createdAt
+    lastUpdatedAt
+    size
+    querySourceListName
+    queryNameSearchTerm
+  }
+}`
 
-router.put('/', (req: Request, res: Response) => {
-   res.send('PUT Request sent to lists')
-})
+/*
+  Get list
+*/
+router.get('/', async (req, res) => {
+  const { primaryPhone, listName } = req.body;
 
-router.delete('/', (req: Request, res: Response) => {
-   res.send('DELETE Request sent to lists')
-})
+ try {
+   // Make the mutation request using Apollo Client
+   const response = await apolloClient.mutate({
+     mutation: LIST_QUERY,
+     variables: { primaryPhone, listName },
+     errorPolicy: 'all'
+   });
+   
+   if (response.data && response.errors) {
+     console.log("Operation partially succeeded:", response.data);
+     console.log("But encountered errors:", response.errors);
+   } else {
+    // Send the response back to the client
+     res.json(response.data);
+   }   
+ } catch (error) {
+    console.log(error)
+ }   
+});
+
+/*
+   Edit/create list
+*/
+router.post('/', async (req, res) => {
+    const { primaryPhone, listQuery } = req.body;
+
+   try {
+     // Make the mutation request using Apollo Client
+     const response = await apolloClient.mutate({
+       mutation: LIST_MUTATION,
+       variables: { primaryPhone, listQuery },
+     });
+
+     if (response.data && response.errors) {
+       console.log("Operation partially succeeded:", response.data);
+       console.log("But encountered errors:", response.errors);
+     } else {
+      // Send the response back to the client
+       res.json(response.data);
+     }
+     
+   } catch (error) {
+      console.log(error)
+   }   
+ });
 
 export default router;
