df7a0954dad9018b57570cb25622d6225898d843
Daniel Kim <hyunkim03@gmail.com>
2024-03-07 06:56:38 -0800

    Added message and conversation queries

--- backend/src/routes/messages.ts
+++ backend/src/routes/messages.ts
@@ -12,25 +12,236 @@ const MESSAGE_MUTATION = gql`
   mutation createMessage($to: String!, $from: String!, $body: String!, $media: [String!], $passthrough: String) {
     createMessage(to: $to, from: $from, body: $body, media: $media, passthrough: $passthrough) {
       id
-      legacyId
-      toNumber
-      fromNumber
-      status
-      statusDescription
-      media
+    legacyId
+    account {
+      id
+      organization {
+        id
+        companyName
+        domain
+        size
+        industry
+        createdAt
+        lastUpdatedAt
+        isOnFreeTrial
+        isActive
+      }
+      createdBy
+      name
+      primaryPhone
+      createdAt
+      lastUpdatedAt
+      callForwardingNumber
+      location
+    }
+    organization {
+      id
+      companyName
+      domain
+      size
+      industry
+      createdAt
+      lastUpdatedAt
+      isOnFreeTrial
+      isActive
+    }
+    toNumber
+    fromNumber
+    status
+    statusDescription
+    media
+    body
+    sentAt
+    confirmedAt
+    wave {
+      id
+      scheduledTime
+      name
+      account {
+        id
+        organization {
+            id
+            companyName
+            domain
+            size
+            industry
+            createdAt
+            lastUpdatedAt
+            isOnFreeTrial
+            isActive
+        }
+        createdBy
+        name
+        primaryPhone
+        createdAt
+        lastUpdatedAt
+        callForwardingNumber
+        location
+      }
+      organization {
+            id
+            companyName
+            domain
+            size
+            industry
+            createdAt
+            lastUpdatedAt
+            isOnFreeTrial
+            isActive
+      }
       body
-      sentAt
-      confirmedAt
-      errorCode
+      media
+      sentBy
+      lists {
+        id
+        createdBy
+        name
+        createdAt
+        lastUpdatedAt
+      }
+      totalMessages
+      totalMessagesDelivered
+      totalMessagesFailed
+      type
+    }
+    contact {
+      id
+    }
+    errorCode
     }
   }
+`;
+
+const MESSAGE_QUERY = gql`
+query message($id: ID!) {
+  message(id: $id) {
+    id
+    legacyId
+    account {
+      id
+      organization {
+        id
+        companyName
+        domain
+        size
+        industry
+        createdAt
+        lastUpdatedAt
+        isOnFreeTrial
+        isActive
+      }
+      createdBy
+      name
+      primaryPhone
+      createdAt
+      lastUpdatedAt
+      callForwardingNumber
+      location
+    }
+    organization {
+      id
+      companyName
+      domain
+      size
+      industry
+      createdAt
+      lastUpdatedAt
+      isOnFreeTrial
+      isActive
+    }
+    toNumber
+    fromNumber
+    status
+    statusDescription
+    media
+    body
+    sentAt
+    confirmedAt
+    wave {
+      id
+      scheduledTime
+      name
+      account {
+        id
+        organization {
+            id
+            companyName
+            domain
+            size
+            industry
+            createdAt
+            lastUpdatedAt
+            isOnFreeTrial
+            isActive
+        }
+        createdBy
+        name
+        primaryPhone
+        createdAt
+        lastUpdatedAt
+        callForwardingNumber
+        location
+      }
+      organization {
+            id
+            companyName
+            domain
+            size
+            industry
+            createdAt
+            lastUpdatedAt
+            isOnFreeTrial
+            isActive
+      }
+      body
+      media
+      sentBy
+      lists {
+        id
+        createdBy
+        name
+        createdAt
+        lastUpdatedAt
+      }
+      totalMessages
+      totalMessagesDelivered
+      totalMessagesFailed
+      type
+    }
+    contact {
+      id
+    }
+    errorCode
+  }
+}
+`;
+
+const CONVO_QUERY = gql`
+query conversation(
+  $to: String!,
+  $from: String!,
+  $before: String
+) {
+  conversation(
+    to: $to,
+    from: $from,
+    before: $before
+  ) {
+    messages {
+      id
+    }
+    pageInfo {
+      totalItems
+    }
+  }
+}
 `;
 
 /*
 ERROR: Issue with failure to retrieve error code in graphQLErrors
 WILL FIX :D
 */
-router.post('/', async (req, res) => {
+router.post('/send', async (req, res) => {
     const { to, from, body } = req.body;
     const media : string[] = []
     const passthrough = ""
@@ -79,16 +290,99 @@ router.post('/', async (req, res) => {
     
   });
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('GET Request sent to messages')
-})
+  router.post('/get', async (req, res) => {
+    const {id} = req.body;
+    
+    try {
+      // Make the mutation request using Apollo Client
+      const response = await apolloClient.query({
+        query: MESSAGE_QUERY,
+        variables: { id },
+        errorPolicy: 'all'
+      });
+      res.json(response.data);
+      
+  
+      // Send the response back to the client
+      
+    } catch (error) {
+      // TypeScript knows error is of type 'unknown', so we narrow it down
+      if (error instanceof ApolloError) {
+        if (error.networkError) {
+          console.error("Network error:", error.networkError);
+          res.status(500).json({ error: "Network error occurred" });
+        } else if (error.graphQLErrors) {
+          error.graphQLErrors.forEach((err) => {
+            console.error("GraphQL error:", err.message);
+          });
+          // Respond with the first GraphQL error message for simplicity
+          res.status(500).json({ error: error.graphQLErrors[0]?.message || "GraphQL error occurred" });
+        } else {
+          // Handle other ApolloError cases
+          res.status(500).json({ error: "An Apollo client error occurred" });
+        }
+      } else {
+        // For non-Apollo errors, you might still want to check if it's an instance of Error
+        if (error instanceof Error) {
+          console.error("An error occurred:", error.message);
+          res.status(500).json({ error: error.message });
+        } else {
+          // Fallback for when the error isn't an instance of Error
+          console.error("An unknown error occurred:", error);
+          res.status(500).json({ error: "An unknown error occurred" });
+        }
+      }
+    }
+    
+  });
 
-router.put('/', (req: Request, res: Response) => {
-    res.send('PUT Request sent to messages')
-})
 
-router.delete('/', (req: Request, res: Response) => {
-    res.send('DELETE Request sent to messages')
-})
+  router.post('/getConvo', async (req, res) => {
+    const {to, from} = req.body
+    const before = null
+    
+    try {
+      // Make the mutation request using Apollo Client
+      const response = await apolloClient.query({
+        query: CONVO_QUERY,
+        variables: { to, from, before },
+        errorPolicy: 'all'
+      });
+      res.json(response.data);
+      
+  
+      // Send the response back to the client
+      
+    } catch (error) {
+      // TypeScript knows error is of type 'unknown', so we narrow it down
+      if (error instanceof ApolloError) {
+        if (error.networkError) {
+          console.error("Network error:", error.networkError);
+          res.status(500).json({ error: "Network error occurred" });
+        } else if (error.graphQLErrors) {
+          error.graphQLErrors.forEach((err) => {
+            console.error("GraphQL error:", err.message);
+          });
+          // Respond with the first GraphQL error message for simplicity
+          res.status(500).json({ error: error.graphQLErrors[0]?.message || "GraphQL error occurred" });
+        } else {
+          // Handle other ApolloError cases
+          res.status(500).json({ error: "An Apollo client error occurred" });
+        }
+      } else {
+        // For non-Apollo errors, you might still want to check if it's an instance of Error
+        if (error instanceof Error) {
+          console.error("An error occurred:", error.message);
+          res.status(500).json({ error: error.message });
+        } else {
+          // Fallback for when the error isn't an instance of Error
+          console.error("An unknown error occurred:", error);
+          res.status(500).json({ error: "An unknown error occurred" });
+        }
+      }
+    }
+    
+  });
+
 
 export default router;
